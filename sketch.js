ab4dc30631690a2ab69bb0a8afd82708fd8db207
Komalvijayshinde <96244070+Komalvijayshinde@users.noreply.github.com>
2021-12-28 19:30:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,97 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var b, enemyImage, ballImage, scene, ball, alien1, alien2, alien3, score=0, opscore=0, speed=5
 
-function preload() {
-  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-  trex_collided = loadImage("trex_collided.png");
-
-  groundImage = loadImage("ground2.png")
+function preload(){
+  
+  b = loadImage("b.png");
+  enemyImage = loadImage("alien.png");
+  ballImage = loadImage("ball.png");
+  
 }
 
-function setup() {
-createCanvas(600, 200);
 
-//create a trex sprite
-trex = createSprite(50,160,20,50);
-trex.addAnimation("running", trex_running);
-trex.scale = 0.5;
+
+function setup() {
+  createCanvas(400, 400);
   
-//create a ground sprite
-ground = createSprite(200,180,400,20);
-ground.addImage("ground",groundImage);
-ground.x = ground.width /2;
-ground.velocityX = -4;  
-invisibleGround=createSprite(200,190,400,10);
-invisibleGround.visible=false;
+  scene = createSprite(200,200);
+  scene.addImage(b)
+  ball = createSprite(200,345,200,345)
+  ball.addImage(ballImage)
+  ball.scale=0.1
+
+  alien1 = createSprite(200,280,10,10)
+  alien2 = createSprite(200,170,10,10)
+  alien3 = createSprite(200,50,10,10)
+  alien1.addImage(enemyImage)
+  alien2.addImage(enemyImage)
+  alien3.addImage(enemyImage)
+
+  alien1.scale=0.3
+  alien2.scale=0.3
+  alien3.scale=0.3
+
+
+  
+   score = 0    
 }
 
 function draw() {
-background(220);
+ background(0);
+ createEdgeSprites()
 
-//jump when the space button is pressed
-if (keyDown("space") && trex.y>=100) {
-  trex.velocityY = -10;
+ alien1.x=alien1.x+speed
+ alien2.x=alien2.x-speed
+ alien3.x=alien3.x+speed
+
+ if(alien1.x<0 || alien1.x>width)
+ {
+   speed=speed*-1
+ }
+
+
+
+if(keyDown(UP_ARROW)){
+  ball.y=ball.y-3
 }
 
-trex.velocityY = trex.velocityY + 0.8
-
-if (ground.x < 0) {
-  ground.x = ground.width / 2;
+if(keyDown(DOWN_ARROW)){
+  ball.y=ball.y+3
 }
 
-trex.collide(invisibleGround);
+if(keyDown(LEFT_ARROW)){
+  ball.x=ball.x-3
+}
+
+if(keyDown(RIGHT_ARROW)){
+  ball.x=ball.x+3
+}
+
+if(ball.isTouching(alien1)|| ball.isTouching(alien2)|| ball.isTouching(alien3)){
+    ball.x=200
+  ball.y=350
+  opscore = opscore+1
+}
+if(ball.y<0){
+  ball.x=200
+  ball.y=345
+  score=score+1
+}
+
+
 drawSprites();
+textSize(15)
+  fill("Yellow")
+  text("Humans:"+score,328,30);
+  
+
+textSize(15)
+  fill("Yellow")
+  text("Aliens:"+opscore,5,30);
+  
+
+    
+
 }
+
+
+
